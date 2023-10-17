65a2a30f9bbc8073e8fe86f18b65a4b0fdb96be6
Ali <the.alif.dev@gmail.com>
2023-10-17 11:21:30 +0330

    Move style codes to css file

--- src/App.vue
+++ src/App.vue
@@ -130,15 +130,3 @@ export default defineComponent({
   }
 });
 </script>
-
-<style scoped>
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-</style>
